5907606be3b9184ae28b33f642370903bf80bf9c
Aneel Grewal <aneel.grewal@hotmail.com>
2023-05-30 12:24:36 +0200

    passing props from Game component to Square component

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,21 +5,21 @@ const Game = () => {
     <div className='w-screen h-screen bg-red-200 flex justify-center items-center'>
         <div className='w-[240px] border-2 border-gray-600 bg-green-200'>
             <div className='flex'>
-                <Square />
-                <Square />
-                <Square />
+                <Square value={1}/>
+                <Square value={1}/>
+                <Square value={1}/>
             </div>
 
             <div className='flex'>
-                <Square />
-                <Square />
-                <Square />
+                <Square value={1}/>
+                <Square value={1}/>
+                <Square value={1}/>
             </div>
 
             <div className='flex'>
-                <Square />
-                <Square />
-                <Square />
+                <Square value={1}/>
+                <Square value={1}/>
+                <Square value={1}/>
             </div>
             
         </div>
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,9 @@
 
 
-const Square = () => {
+const Square = ({value}) => {
     return(
         <div className="w-20 h-20 bg-slate-100 border-2 border-spacing-5 border-gray-600">
-            <p className="h-full flex justify-center items-center text-6xl font-bold text-gray-700">X</p>
+            <p className="h-full flex justify-center items-center text-6xl font-bold text-gray-700">{value}</p>
         </div>
     )
 }
